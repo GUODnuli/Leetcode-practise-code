fba602a78969743d03cf78f7bfae78e984afc465
zhuangxiaosen <624115642@qq.com>
2024-03-15 14:49:59 +0800

    正则表达式匹配，完成。

--- src/10.正则表达式匹配.rs
+++ src/10.正则表达式匹配.rs
@@ -68,16 +68,29 @@ impl Solution {
     pub fn is_match(s: String, p: String) -> bool {
         let s = s.chars().collect::<Vec<char>>();
         let p = p.chars().collect::<Vec<char>>();
-
-        let index_s = 0;
-        let index_p = 0;
-
-        while index_s < s.len() && index_p < p.len() {
-            
+        let (s_len, p_len) = (s.len(), p.len());
+        let mut dp = vec![vec![false; p_len + 1]; s_len + 1];
+        dp[0][0] = true;
+        for i in 2..=p_len {
+            if p[i - 1] == '*' {
+                dp[0][i] = dp [0][i - 2];
+            }
+        }
+        for i in 1..=s_len {
+            for j in 1..=p_len {
+                if p[j - 1] == '.' || p[j - 1] == s[i - 1] {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if p[j - 1] == '*' {
+                    if p[j - 2] != s[i - 1] && p[j - 2] != '.' {
+                        dp[i][j] = dp[i][j - 2];
+                    } else {
+                        dp[i][j] = dp[i - 1][j] || dp[i][j - 2];
+                    }
+                }
+            }
         }
 
-        false
+        dp[s_len][p_len]
     }
 }
 // @lc code=end
-
