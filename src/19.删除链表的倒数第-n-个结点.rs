baa76e245f2e616fb1e48c49302f52175f9aed6b
Xiaosen Zhuang <624115642@qq.com>
2024-03-25 16:40:58 +0800

    删除链表的倒数第n个节点，进行中。

---
+++ src/19.删除链表的倒数第-n-个结点.rs
@@ -0,0 +1,83 @@
+/*
+ * @lc app=leetcode.cn id=19 lang=rust
+ *
+ * [19] 删除链表的倒数第 N 个结点
+ *
+ * https://leetcode.cn/problems/remove-nth-node-from-end-of-list/description/
+ *
+ * algorithms
+ * Medium (47.54%)
+ * Likes:    2834
+ * Dislikes: 0
+ * Total Accepted:    1.4M
+ * Total Submissions: 2.9M
+ * Testcase Example:  '[1,2,3,4,5]\n2'
+ *
+ * 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：head = [1,2,3,4,5], n = 2
+ * 输出：[1,2,3,5]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：head = [1], n = 1
+ * 输出：[]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：head = [1,2], n = 1
+ * 输出：[1]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 链表中结点的数目为 sz
+ * 1 <= sz <= 30
+ * 0 <= Node.val <= 100
+ * 1 <= n <= sz
+ * 
+ * 
+ * 
+ * 
+ * 进阶：你能尝试使用一趟扫描实现吗？
+ * 
+ */
+
+// @lc code=start
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+  pub val: i32,
+  pub next: Option<Box<ListNode>>
+}
+
+impl ListNode {
+  #[inline]
+  fn new(val: i32) -> Self {
+    ListNode {
+      next: None,
+      val
+    }
+  }
+}
+use crate::solution::Solution;
+
+impl Solution {
+    pub fn remove_nth_from_end(head: Option<Box<ListNode>>, n: i32) -> Option<Box<ListNode>> {
+        let 
+    }
+}
+// @lc code=end
+
--- src/lib.rs
+++ src/lib.rs
@@ -17,3 +17,4 @@ pub mod three_sum;
 pub mod three_sum_closest;
 pub mod letter_combinations_of_a_phone_number;
 pub mod four_sum;
+pub mod remove_nth_node_from_end_of_list;
