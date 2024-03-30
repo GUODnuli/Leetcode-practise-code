545955da5f7224dd36ad59e9406591477fb1a999
GUODnuli <624115642@qq.com>
2024-03-31 00:31:32 +0800

    合并有序链表，进行中。

--- src/21.合并两个有序链表.rs
+++ src/21.合并两个有序链表.rs
@@ -52,24 +52,37 @@
 
 // @lc code=start
 // Definition for singly-linked list.
-// #[derive(PartialEq, Eq, Clone, Debug)]
-// pub struct ListNode {
-//   pub val: i32,
-//   pub next: Option<Box<ListNode>>
-// }
-//
-// impl ListNode {
-//   #[inline]
-//   fn new(val: i32) -> Self {
-//     ListNode {
-//       next: None,
-//       val
-//     }
-//   }
-// }
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+  pub val: i32,
+  pub next: Option<Box<ListNode>>
+}
+
+impl ListNode {
+  #[inline]
+  fn new(val: i32) -> Self {
+    ListNode {
+      next: None,
+      val
+    }
+  }
+}
+
+use crate::solution::Solution;
+
 impl Solution {
     pub fn merge_two_lists(list1: Option<Box<ListNode>>, list2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        
+        if list1.is_none() {
+            return list2;
+        }
+        if list2.is_none() {
+            return list1;
+        }
+        let mut dummy = Some(Box::new(ListNode{ val: 0, next: list1 }));
+        let mut ptr1 = &mut dummy;
+        let mut ptr2 = list2;
+
+        dummy.unwrap().next
     }
 }
 // @lc code=end
