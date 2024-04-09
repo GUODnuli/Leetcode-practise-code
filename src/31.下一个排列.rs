89168545114db963397e31e4ede36d6f2f99d145
Xiaosen Zhuang <624115642@qq.com>
2024-04-09 12:00:58 +0800

    下一个排列，进行中。

---
+++ src/31.下一个排列.rs
@@ -0,0 +1,96 @@
+/*
+ * @lc app=leetcode.cn id=31 lang=rust
+ *
+ * [31] 下一个排列
+ *
+ * https://leetcode.cn/problems/next-permutation/description/
+ *
+ * algorithms
+ * Medium (39.13%)
+ * Likes:    2468
+ * Dislikes: 0
+ * Total Accepted:    505.4K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 整数数组的一个 排列  就是将其所有成员以序列或线性顺序排列。
+ * 
+ * 
+ * 例如，arr = [1,2,3] ，以下这些都可以视作 arr 的排列：[1,2,3]、[1,3,2]、[3,1,2]、[2,3,1] 。
+ * 
+ * 
+ * 整数数组的 下一个排列 是指其整数的下一个字典序更大的排列。更正式地，如果数组的所有排列根据其字典顺序从小到大排列在一个容器中，那么数组的 下一个排列
+ * 就是在这个有序容器中排在它后面的那个排列。如果不存在下一个更大的排列，那么这个数组必须重排为字典序最小的排列（即，其元素按升序排列）。
+ * 
+ * 
+ * 例如，arr = [1,2,3] 的下一个排列是 [1,3,2] 。
+ * 类似地，arr = [2,3,1] 的下一个排列是 [3,1,2] 。
+ * 而 arr = [3,2,1] 的下一个排列是 [1,2,3] ，因为 [3,2,1] 不存在一个字典序更大的排列。
+ * 
+ * 
+ * 给你一个整数数组 nums ，找出 nums 的下一个排列。
+ * 
+ * 必须 原地 修改，只允许使用额外常数空间。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：nums = [1,2,3]
+ * 输出：[1,3,2]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：nums = [3,2,1]
+ * 输出：[1,2,3]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：nums = [1,1,5]
+ * 输出：[1,5,1]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= nums.length <= 100
+ * 0 <= nums[i] <= 100
+ * 
+ * 
+ */
+
+// @lc code=start
+// use crate::solution::Solution;
+
+impl Solution {
+    pub fn next_permutation(nums: &mut Vec<i32>) {
+        let mut is_lastest = true;
+        for initial_index in 0..nums.len() {
+            let (mut max_num, mut index) = (nums[initial_index], initial_index);
+            let mut is_change = false;
+            for (i, v) in nums.iter().enumerate() {
+                if max_num < *v {
+                    (index, max_num) = (i, *v);
+                    is_change = true;
+                    is_lastest = false;
+                }
+            }
+            if is_change {
+                (nums[index], nums[index - 1]) = (nums[index - 1], nums[index]);
+                return;
+            }
+        }
+        if is_lastest {
+            nums.sort();
+        }
+    }
+}
+// @lc code=end
+
