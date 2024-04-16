c54a81082504f9f614b1fb3f281ba528fd1c01d9
Xiaosen Zhuang <624115642@qq.com>
2024-04-16 15:39:26 +0800

    最长有效括号，完成。

--- src/32.最长有效括号.rs
+++ src/32.最长有效括号.rs
@@ -56,7 +56,7 @@
  */
 
 // @lc code=start
-// use crate::solution::Solution;
+use crate::solution::Solution;
 use std::cmp::max;
 
 impl Solution {
@@ -69,15 +69,14 @@ impl Solution {
             if s[i] == ')' {
                 if s[i - 1] == '(' {
                     if i >= 2 {
-                        dp[i] = dp[i - 2]
+                        dp[i] = dp[i - 2] + 2;
                     } else {
                         dp[i] = 2;
                     }
                 } else if i - dp[i - 1] > 0 && s[i - dp[i - 1] - 1] == '(' {
+                    dp[i] = dp[i - 1] + 2;
                     if i - dp[i - 1] >= 2 {
-                        dp[i] = dp[i - dp[i - 1] - 2]
-                    } else {
-                        dp[i] = 2;
+                        dp[i] += dp[i - dp[i - 1] - 2];
                     }
                 }
                 max_len = max(max_len, dp[i]);
